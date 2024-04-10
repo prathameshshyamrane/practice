e2097210b08ff9b8d617aa6693664427211c46b1
prathameshshyamrane <psr261004@gmail.com>
2024-04-10 15:15:09 +0530

    practice

---
+++ assigment/src/assigment/addsub.java
@@ -0,0 +1,16 @@
+package assigment;
+
+public class addsub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		        int num1 = 5;
+		        int num2 = 10;
+		        int sum = num1 + num2;
+		        System.out.println("Sum: " + sum);
+		    }
+		}
+
+
+	
